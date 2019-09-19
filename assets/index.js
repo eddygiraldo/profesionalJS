a0d8efa1568f1528f5b5b4aded0a526d0d679150
Eddy Alexander Giraldo Pinilla <xcbegira@intcomex.com>
2019-09-19 18:42:36 -0500

    plugins

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,14 @@
 import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({
+  el: video, plugins: [
+    //new AutoPlay()
+  ]
+});
 
-const button = document.querySelector('button');
-button.onclick = () => player.togglePlay();
+const buttonPlay = document.getElementById('PlayButton');
+const buttonMute = document.getElementById('MuteButton');
+buttonPlay.onclick = () => player.togglePlay();
+buttonMute.onclick = () => player.toggleMute();
